ae678f8d37f387503845d80b012eed63cdef963e
Chris Palmer <capalmer1013@gmail.com>
2017-07-27 02:10:04 -0400

    I found the secret song. I need to use the threading mixin

--- src/tcpServer/server.py
+++ src/tcpServer/server.py
@@ -12,11 +12,8 @@ def sendOthers(username, message):
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
-    The request handler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
+    This will need to use the threading mixin in order to handle
+    multiple continuous client connections
     """
 
     def handle(self):
@@ -37,16 +34,21 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 
         # self.request is the TCP socket connected to the client
         while time.time() - startTime < connectionTimeout:
-            time.sleep(lastSleep)
-            lastSleep *= 2
-            inputReady, _ , _ = select.select([self.request], [], [])
+            try:
+                time.sleep(lastSleep)
+                lastSleep *= 2
+                inputReady, _ , _ = select.select([self.request], [], [])
 
-            if inputReady:
-                self.data = self.request.recv(1024).strip()
-                print "{} wrote:".format(self.client_address[0])
-                print self.data
-                self.request.sendall(">" + self.data)
-
+                if inputReady:
+                    self.data = self.request.recv(1024).strip()
+                    print "{} wrote:".format(self.client_address[0])
+                    print self.data
+                    self.request.sendall(">" + self.data)
+            
+            except:
+                del [key]
+                return
+        
         self.request.sendall("Connection Timeout.")
 if __name__ == "__main__":
     HOST, PORT = "0.0.0.0", 5000
